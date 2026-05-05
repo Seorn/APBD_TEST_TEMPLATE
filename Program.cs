027c7fe904d6018f95b382abbf4f03a9989da0fc
s30031 <s30031@LAB362-09>
2026-05-05 16:48:41 +0200

    Changed Program.cs for more safe version

--- Program.cs
+++ Program.cs
@@ -1,15 +1,20 @@
+using APBD_TEST_TEMPLATE.Services;
+
+
+
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
 builder.Services.AddEndpointsApiExplorer();
+
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddScoped<IVendorService, VendorService>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -18,10 +23,6 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-app.UseAuthorization();
-
 app.MapControllers();
 
 app.Run();
-
-
