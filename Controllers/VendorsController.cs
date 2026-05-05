f0742c85474b9fc37b6f13fea2951df239c07316
s30031 <s30031@LAB362-09>
2026-05-05 16:44:04 +0200

    VendorsController.cs configuration done

--- Controllers/VendorsController.cs
+++ Controllers/VendorsController.cs
@@ -1,6 +1,51 @@
-﻿namespace APBD_TEST_TEMPLATE.Controllers
+﻿using APBD_TEST_TEMPLATE.DTOs;
+using APBD_TEST_TEMPLATE.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace APBD_TEST_TEMPLATE.Controllers
 {
-    public class VendorsController
+    [ApiController]
+    [Route("api/[controller]")]
+
+    public class VendorsController : ControllerBase
     {
+        private readonly IVendorService _vendorService;
+
+        public VendorsController(IVendorService vendorService)
+
+        {
+            _vendorService = vendorService;
+        }
+
+
+        [HttpGet("{code}")]
+        public async Task<IActionResult> GetVendor(string code)
+        {
+            var vendor = await _vendorService.GetVendorAsync(code);
+
+            if (vendor == null)
+            {
+                return NotFound($"Vendor {code} not found.");
+            }
+
+            return Ok(vendor);
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> CreateVendor(CreateVendorRequest request)
+        {
+            var result = await _vendorService.CreateVendorAsync(request);
+
+            if (!result)
+            {
+                return BadRequest("Failed");
+            }
+
+            return StatusCode(201);
+        }
+
+
     }
+
 }
